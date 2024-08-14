d11903d9fcafbd2d2de121edc9a98d1db4580351
Shania Lopez <slopez@fandm.edu>
2024-08-13 20:23:49 -0400

    completed mock code challenge

--- server/app.py
+++ server/app.py
@@ -19,12 +19,86 @@ app.json.compact = False
 migrate = Migrate(app, db)
 
 db.init_app(app)
+api = Api(app)
 
 
 @app.route('/')
 def home():
     return ''
 
+class Scientists(Resource):
+    def get(self):
+        scientists = db.session.query(Scientist).all()
+        return [scientist.to_dict(only=('id', 'name', 'field_of_study')) for scientist in scientists], 200
+    def post(self):
+        name = request.get_json()['name']
+        field_of_study = request.get_json()['field_of_study']
+
+        try:
+            scientist = Scientist(name=name, field_of_study=field_of_study)
+
+            db.session.add(scientist)
+            db.session.commit()
+            
+            return scientist.to_dict(), 201
+        except:
+            return {'errors': ['validation errors']}, 400
+
+
+class ScientistById(Resource): 
+    def get(self, id):
+        scientist = db.session.query(Scientist).filter(Scientist.id ==id).first()
+        if scientist:
+            return scientist.to_dict(), 200
+        return {'error': 'Scientist not found'}, 404
+    def delete(self, id):
+        scientist = db.session.query(Scientist).filter(Scientist.id ==id).first()
+        if scientist:
+            db.session.delete(scientist)
+            db.session.commit()
+            return {}, 204
+        return {'error': 'Scientist not found'}, 404
+    def patch(self, id):
+        scientist = db.session.query(Scientist).filter(Scientist.id == id).first()
+        if scientist:
+            try:
+                for attr in request.get_json():
+                    setattr(scientist, attr, request.get_json()[attr])
+                db.session.add(scientist)
+                db.session.commit()
+
+                return scientist.to_dict(), 202
+            except:
+                return {'errors': ['validation errors']}, 400
+            
+        return {'error': 'Scientist not found'}, 404
+
+class Planets(Resource):
+    def get(self):
+        planets = db.session.query(Planet).all()
+        return [planet.to_dict(only=('id', 'name', 'distance_from_earth', 'nearest_star')) for planet in planets], 200
+
+class Missions(Resource):
+    def post(self):
+        name = request.get_json()['name']
+        scientist_id = request.get_json()['scientist_id']
+        planet_id = request.get_json()['planet_id']
+        
+        try:
+            mission = Mission(name=name, scientist_id=scientist_id, planet_id=planet_id)
+            db.session.add(mission)
+            db.session.commit()
+
+            return mission.to_dict(), 201
+        except:
+            return {'errors': ['validation errors']}, 400
+            
+
+
+api.add_resource(Missions, '/missions')
+api.add_resource(Scientists, '/scientists')
+api.add_resource(ScientistById, '/scientists/<int:id>')
+api.add_resource(Planets, '/planets')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -26,35 +26,67 @@ class Planet(db.Model, SerializerMixin):
     nearest_star = db.Column(db.String)
 
     # Add relationship
+    missions = db.relationship('Mission', back_populates='planet', cascade='all, delete-orphan')
 
     # Add serialization rules
+    serialize_rules = ('-missions.planet',)
 
 
 class Scientist(db.Model, SerializerMixin):
     __tablename__ = 'scientists'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    field_of_study = db.Column(db.String)
+    name = db.Column(db.String, nullable=False)
+    field_of_study = db.Column(db.String, nullable=False)
 
     # Add relationship
+    missions = db.relationship('Mission', back_populates='scientist', cascade='all, delete-orphan')
 
     # Add serialization rules
+    serialize_rules = ('-missions.scientist',)
 
     # Add validation
+    @validates('name', 'field_of_study')
+    def validates_name_and_field(self, key, attr):
+        if attr == None:
+            raise ValueError("Field cannot be empty.")
+        elif len(attr) == 0:
+            raise ValueError("Field cannot be empty.")
+
+        return attr
+        
 
 
 class Mission(db.Model, SerializerMixin):
     __tablename__ = 'missions'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
+    name = db.Column(db.String, nullable=False)
+    planet_id = db.Column(db.Integer, db.ForeignKey('planets.id'), nullable=False)
+    scientist_id = db.Column(db.Integer, db.ForeignKey('scientists.id'), nullable=False)
 
     # Add relationships
-
+    planet = db.relationship('Planet', back_populates='missions')
+    scientist = db.relationship('Scientist', back_populates='missions')
     # Add serialization rules
+    serialize_rules = ('-scentist.missions', '-planet.missions',)
 
     # Add validation
+
+    @validates('name')
+    def validates_name(self, key, name):
+        if name == None:
+            raise ValueError("Field cannot be empty.")
+        elif len(name) == 0:
+            raise ValueError("Field cannot be empty.")
+        return name
+    
+    @validates('scientist_id', 'planet_id')
+    def validates_scientist_id(self, key, id):
+        if id == None:
+            raise ValueError("ID cannot be empty.")
+        return id
+
 
 
 # add any models you may need.
